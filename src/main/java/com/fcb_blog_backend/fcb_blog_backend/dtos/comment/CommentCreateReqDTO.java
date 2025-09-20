d8d4483fb6b3971269d04cfe7ef0411fc15786a3
Filipe Pereira <filipepereira977@gmail.com>
2025-09-20 20:32:49 +0200

    fix: comment validators

--- src/main/java/com/fcb_blog_backend/fcb_blog_backend/dtos/comment/CommentCreateReqDTO.java
+++ src/main/java/com/fcb_blog_backend/fcb_blog_backend/dtos/comment/CommentCreateReqDTO.java
@@ -1,6 +1,7 @@
 package com.fcb_blog_backend.fcb_blog_backend.dtos.comment;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CommentCreateReqDTO(@NotBlank String content, @NotBlank int userId, @NotBlank int postId) {
+public record CommentCreateReqDTO(@NotBlank String content, @NotNull int userId, @NotNull int postId) {
 }
